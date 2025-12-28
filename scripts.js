0e2725b8ca804c515a56c9de0ef50a9e3475429f
renangondo <renanluizgondo@gmail.com>
2025-12-28 11:24:19 -0300

    Adicionado mais opções de conversões

--- scripts.js
+++ scripts.js
@@ -1,11 +1,14 @@
 const convertButton = document.querySelector(".convert-button");
 const currencySelect = document.querySelector(".currency-select");
+const convertedToSelect = document.querySelector(".converted-to-select");
 
 function convertValues() {
     // Lógica de conversão de moedas
     const inputCurrencyValue = document.querySelector(".input-currency").value;
-    const currencyValueToConvert = document.querySelector(".currency-value-to-convert"); // elemento p para mostrar valor em real
+    const currencyValueToConvert = document.querySelector(".currency-value-to-convert");
     const currencyValueConverted = document.querySelector(".currency-value"); // elemento p para mostrar valor convertido
+    
+
 
     const dolarToday = 5.2; // Exemplo de valor do dólar
     const euroToday = 5.9; // Exemplo de valor do euro
@@ -23,7 +26,7 @@ function convertValues() {
             style: "currency", //mostra que é moeda
             currency: "EUR" //moeda em euro
         }).format(inputCurrencyValue / euroToday); /* Valor em euro */
- 
+
     }
 
     currencyValueToConvert.innerHTML = new Intl.NumberFormat("pt-BR", {
@@ -33,6 +36,42 @@ function convertValues() {
 
 
 }
+
+function convertedTo() {
+    const currencyName = document.querySelector(".currency");
+    const currencyImage = document.querySelector(".icone");
+
+    if (convertedToSelect.value == "real") {
+    currencyName.innerHTML = "Real Brasileiro";
+    currencyImage.src = "imagens/real.png";
+    }
+
+
+    if (convertedToSelect.value == "dolar") {
+    currencyName.innerHTML = "Dólar americano";
+    currencyImage.src = "imagens/dolar.png";
+    }
+
+    if (convertedToSelect.value == 'euro') {
+    currencyName.innerHTML = "Euro";
+    currencyImage.src = "imagens/euro.png";
+    }
+
+    if (convertedToSelect.value == 'libra') {
+    currencyName.innerHTML = "Libra Esterlina";
+    currencyImage.src = "imagens/libra.png";
+    }
+
+    if (convertedToSelect.value == 'bitcoin') {
+    currencyName.innerHTML = "Bitcoin";
+    currencyImage.src = "imagens/bitcoin.png";
+    }
+
+    convertValues();
+
+}   
+
+
 
 function changeCurrency() {
     const currencyName = document.getElementById("currency-name");
@@ -47,11 +86,21 @@ function changeCurrency() {
         currencyImage.src = "imagens/euro.png";
     }
 
+    if (currencySelect.value == 'libra') {
+        currencyName.innerHTML = "Libra Esterlina";
+        currencyImage.src = "imagens/libra.png";
+    }
+
+    if (currencySelect.value == 'bitcoin') {
+        currencyName.innerHTML = "Bitcoin";
+        currencyImage.src = "imagens/bitcoin.png";
+    }
+
     convertValues();
-    
+
 }
 
 
-
+convertedToSelect.addEventListener("change", convertedTo);
 currencySelect.addEventListener("change", changeCurrency);
-convertButton.addEventListener("click", convertValues); 
+convertButton.addEventListener("click", convertValues);
